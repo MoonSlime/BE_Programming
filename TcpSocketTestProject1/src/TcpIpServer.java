472020e56ebe3da829c62e98fc342c7ad28c3553
Chan K <sck3578@naver.com>
2020-01-09 13:19:03 +0900

    .
    .

--- TcpSocketTestProject1/src/TcpIpServer.java
+++ TcpSocketTestProject1/src/TcpIpServer.java
@@ -14,7 +14,7 @@ public class TcpIpServer {
 		try(ServerSocket serverSocket = new ServerSocket(8080)){
             while(true) {
                 // 요청이 들어올때까지 대기, 클라이언트의 연결요청이 들어오면 클라이언트소켓과 통신할 소켓을 반환
-                Socket socket = serverSocket.accept(); 
+                Socket socket = serverSocket.accept(); //listen.
                 LOG.info(socket.getInetAddress() +"로부터 연결요청이 들어왔습니다.");
                 
                 // 출력스트림 
@@ -24,8 +24,7 @@ public class TcpIpServer {
                 dos.writeUTF("Message from server");
                 LOG.info("메시지 전송 완료");
                 
-                dos.close();
-                
+                dos.close();         
             }
         }catch(IOException e) {
         	LOG.severe(e.toString());
