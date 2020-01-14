fff5d2cd8697721dbd9984620232c9c916460664
chankyu.sim <sck3578@naver.com>
2020-01-14 17:39:26 +0900

    JwtTokenUtil Setting Finish.

--- ChatServer_Mysql/src/main/java/com/navercorp/chat/Application.java
+++ ChatServer_Mysql/src/main/java/com/navercorp/chat/Application.java
@@ -1,5 +1,6 @@
 package com.navercorp.chat;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,7 @@ import org.springframework.boot.web.servlet.context.WebApplicationContextServlet
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 import com.navercorp.chat.mvc.model.UserInfo;
+import com.navercorp.chat.service.JwtTokenUtil;
 
 @SpringBootApplication(scanBasePackages = { "com.navercorp.chat.mvc.controller" })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
@@ -29,6 +31,21 @@ public class Application extends WebApplicationContextServletContextAwareProcess
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-//		Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+		UserInfo user = new UserInfo();
+		user.setUserId("chankyu");
+		user.setPassword("password");
+		
+		JwtTokenUtil jwt = new JwtTokenUtil();
+		String token = jwt.generateToken(user);
+		System.out.println(token);
+		
+		Date expirationDate = jwt.getExpirationDateFromToken(token);
+		System.out.println(expirationDate);
+		
+		String userId = jwt.getUserIdFromToken(token);
+		System.out.println(userId);
+		
+		Boolean validate = jwt.validateToken(token, user);
+		System.out.println(validate);
 	}
 }
